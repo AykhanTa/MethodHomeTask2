646228ea28c48235a0a8f62d979d17cdf31fa96a
AykhanTa <aykhanft@code.edu.az>
2024-02-22 00:20:34 +0400

    Add project files.

---
+++ MethodHomeTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodHomeTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(25,120,45));
+        }
+        static int Sum(params int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 10 && arr[i]<=100)
+                {
+                    result += arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
